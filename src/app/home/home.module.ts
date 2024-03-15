0a5a403ca8f3c955f3a24790c7c33f44a9aa5255
SVLucasAndres <lucassangurima2@gmail.com>
2024-03-15 10:55:34 -0500

    first commit

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,10 +1,11 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
 
 import { HomePageRoutingModule } from './home-routing.module';
+import { Media } from '@ionic-native/media/ngx';
 
 
 @NgModule({
@@ -14,6 +15,7 @@ import { HomePageRoutingModule } from './home-routing.module';
     IonicModule,
     HomePageRoutingModule
   ],
-  declarations: [HomePage]
+  declarations: [HomePage],
+  schemas:[CUSTOM_ELEMENTS_SCHEMA]
 })
 export class HomePageModule {}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,40 @@
 import { Component } from '@angular/core';
-
+import lottie, { LottiePlayer } from "lottie-web";
+import { defineElement } from "@lordicon/element";
+import { RangeCustomEvent } from '@ionic/angular';
+import { Media, MediaObject } from '@ionic-native/media/ngx';
+import { DecimalPipe } from '@angular/common';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
-  constructor() {}
-
+  
+  state:boolean = false;
+  lottie:any;
+  constructor(private media: Media) {}
+  
+  pinFormatter(value: number) {
+    return `${value}`;
+  }
+  
+  playSound() {
+    const file: MediaObject = this.media.create('src/media/alert sound.mp3');
+    if(this.state){
+      file.play();
+    }
+  }
+  ngOnInit() {
+    defineElement(lottie.loadAnimation);
+  }
+  onIonKnobMoveEnd(ev: Event) {
+    console.log('ionKnobMoveEnd:', );
+    if(parseInt(String((ev as RangeCustomEvent).detail.value)) <= 50){
+      this.state=false;
+    }else{
+      this.state=true;
+    }
+    this.playSound();
+  }
 }
